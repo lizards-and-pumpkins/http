3f5f0d00494de11dc4bf4bf8eac30a235223c27e
tim <tim.bezhashvyly@gmail.com>
2020-07-21 15:30:48 +0200

    #1: Fix HttpFactory type

--- src/HttpFactory.php
+++ src/HttpFactory.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace LizardsAndPumpkins\Http;
 
+use LizardsAndPumpkins\Core\Factory\Factory;
+use LizardsAndPumpkins\Core\Factory\FactoryTrait;
 use LizardsAndPumpkins\Http\Routing\HttpRouterChain;
 use LizardsAndPumpkins\Http\Routing\ResourceNotFoundRouter;
 
-class HttpFactory
+class HttpFactory implements Factory
 {
+    use FactoryTrait;
+
     public function createResourceNotFoundRouter(): ResourceNotFoundRouter
     {
         return new ResourceNotFoundRouter();
--- tests/Unit/Suites/HttpFactoryTest.php
+++ tests/Unit/Suites/HttpFactoryTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace LizardsAndPumpkins\Http;
 
+use LizardsAndPumpkins\Core\Factory\Factory;
 use LizardsAndPumpkins\Http\Routing\HttpRouterChain;
 use LizardsAndPumpkins\Http\Routing\ResourceNotFoundRouter;
 use PHPUnit\Framework\TestCase;
@@ -13,6 +14,11 @@ use PHPUnit\Framework\TestCase;
  */
 class HttpFactoryTest extends TestCase
 {
+    public function testIsFactory(): void
+    {
+        $this->assertInstanceOf(Factory::class, new HttpFactory);
+    }
+
     public function testReturnsHttpRouterChain(): void
     {
         $this->assertInstanceOf(HttpRouterChain::class, (new HttpFactory)->createHttpRouterChain());
